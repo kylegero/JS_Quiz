20704f7ca4c50e5f6e0727df4e8b2fbe9bc70fee
Kyle Pykonen <jjrokkos@gmail.com>
2020-11-05 17:29:42 -0800

    Add files via upload
    Restarting for clarity.  Initial commit containing beginnings of html and js files

---
+++ index.js
@@ -0,0 +1,26 @@
+var questions = [{
+    question: "What is Javascript?",
+    choices: ["A type of Indonesian calligrahy", "A programming language used for web development", "An excellent latte recipe", "A slighty-modified version of the Java programming language"],
+    answer: "A programming language used for web development"
+},
+{
+    question: "In Javascript, arrays are contained within...",
+    choices: ["brackets []", "curly brackets {}", "parentheses ()", "double backslashes //"],
+    answer: "brackets []"
+},
+{
+    question: "What does an event listener do?",
+    choices: ["It summarizes a major event for busy users who couldn't attend", "It takes in user input and schedules events for convenient times", "It reacts to all events", "It waits for a certain event to occur"],
+    answer: "It waits for a certain event to occur"
+},
+{
+    question: "What does Javascript actually bring to a webpage?",
+    choices: ["It provides the page's general layout", "It adds interactivity and other complex features", "Nothing, but it sure sounds impressive", "Keyloggers and Trojans"],
+    answer: "It adds interactivity and other complex features"
+},
+{
+    question: "Which of the following correctly declares a Javascript varbiable",
+    choices: ["var", "varrybarry", "iamavariable", "variable"],
+    answer: "var"
+}
+]
